6d6648f07703f1dc5ed9453f0353f7b36091b196
Muhammad Akbar Syahwana <makbarsyahwana@gmail.com>
2019-05-25 00:25:27 +0700

    change date validation

--- received/messageReceiced.js
+++ received/messageReceiced.js
@@ -27,7 +27,7 @@ const messageReceived = (event) => {
             return sendingText(senderID, "please tell your brith day in MM/DD/YYYY format")
         }
 
-        case moment(messageText.toString(), 'MM/DD/YYYY',true).isValid() : {
+        case messageText.macth(/^(\d{2,2})(\/)(\d{2,2})\2(\d{4}|\d{4})$/) !== null: {
             return sendingButton(senderID, "do you wants to know how many days till his next birtday?", [{
                             type: 'text',
                             title: 'Yes',
